0642e8be7d62e55673228f7c9247c455483dc778
Ethan Zhao <zhaoey1@uci.edu>
2026-06-29 16:41:56 -0700

    feat: add test script without any input polling

--- tools/sim/test_asl_publish.py
+++ tools/sim/test_asl_publish.py
@@ -2,23 +2,47 @@
 import time
 import cereal.messaging as messaging
 
-pm = messaging.PubMaster(['advisorySpeedLimit'])
 
-i: int     = 0
-speed: int = 0
+class ASLController:
+    def __init__(self):
+        self.speed = 0
+        self.pm = messaging.PubMaster(['advisorySpeedLimit'])
 
-while True:
-  msg = messaging.new_message('advisorySpeedLimit')
-  msg.advisorySpeedLimit.speed = speed
-  msg.valid = True
-  msg.advisorySpeedLimit.valid = True
-  pm.send('advisorySpeedLimit', msg)
-  print("Sent advisorySpeedLimit =", speed)
-  i += 1
-  if i % 15 == 0:
-      speed += 5
-  if speed > 25:
-      print("speed is at max, exiting...")
-      exit()
-  time.sleep(0.5)
+    def _publish_asl_message(self):
+        msg = messaging.new_message('advisorySpeedLimit')
+        msg.advisorySpeedLimit.speed = self.speed
+        msg.valid = True
+        msg.advisorySpeedLimit.valid = True
+        self.pm.send('advisorySpeedLimit', msg)
+        print("[info] Sent advisorySpeedLimit =", self.speed)
 
+    def _adjust_asl(self, amt: int):
+        self.speed += amt
+
+    def _quit_test(self):
+        print("[info] User requested test quit. Come to a stop safely!")
+        exit()
+
+def main():
+    testASL = ASLController()
+
+    testASL._publish_asl_message()
+
+    INPUT_MAP = {
+        '+' : lambda: testASL._adjust_asl(1),
+        '++': lambda: testASL._adjust_asl(5),
+        '-' : lambda: testASL._adjust_asl(-1),
+        '--': lambda: testASL._adjust_asl(-5),
+        'q' : lambda: testASL._quit_test(),
+    }
+
+    while True:
+        inpt: str = input("[info] Enter a command: ")
+        if inpt not in INPUT_MAP:
+            print("[ERR!] command not found.")
+            continue
+        INPUT_MAP[inpt]()
+        testASL._publish_asl_message()
+
+if __name__ == "__main__":
+    main()
